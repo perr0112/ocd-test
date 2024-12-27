95dc6850aba7ccefaa33db8eaadd396a2ef43b69
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-12-27 17:23:15 +0100

    refactor: routes updated

--- routes/person.php
+++ routes/person.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonController;
 
-Route::resource('people', PersonController::class);
+Route::get('peopleWithCreator', [PersonController::class, 'index']);
+Route::get('people/{id}', [PersonController::class, 'show']);
