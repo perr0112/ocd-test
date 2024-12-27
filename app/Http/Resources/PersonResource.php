f3130ccc94bc397b1e78143296c0a0f609d3fe19
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-12-27 17:08:29 +0100

    feat: personResource to display firstname and lastname of the creator

---
+++ app/Http/Resources/PersonResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PersonResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'birth_name' => $this->birth_name,
+            'middle_names' => $this->middle_names,
+            'date_of_birth' => $this->date_of_birth,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'creator' => $this->creator->first_name . ' ' . $this->creator->last_name,
+        ];
+    }
+}
