bad2dee9857557a39deec977fe14a4941aabc5cf
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-12-27 16:22:01 +0100

    test: route to check correct data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Models\Person;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-relations', function () {
+    $parent = Person::find(1);
+
+    $children = $parent->children;
+    $parents = $parent->parents;
+
+    return [
+        'parent' => $parent,
+        'children' => $children,
+        'parents' => $parents,
+    ];
 });
